8937a627b7f232af8e2e059c69490d0789720727
Jalil <benharkatdjalil@gmail.com>
2023-11-12 15:09:10 +0400

    added postgresql db

--- farm_app_task/settings.py
+++ farm_app_task/settings.py
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
