b2255cbd54ebb493476a6d742e40cb2429649d2d
Jalil <benharkatdjalil@gmail.com>
2023-11-28 13:52:41 +0400

    Added dj_database_url for heroku login to postgresql

--- farm_app_task/settings.py
+++ farm_app_task/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     #     "ENGINE": "django.db.backends.sqlite3",
     #     "NAME": BASE_DIR / "db.sqlite3",
     # }
-    {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
+    {"default": dj_database_url.config(default=config("DATABASE_URL"))}
 }
 
 
